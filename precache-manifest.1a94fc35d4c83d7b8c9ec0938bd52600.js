56e6789d41767bd6eedebf3170ff577845a1185d
Tyler Frick <tyfrick25@gmail.com>
2020-10-19 23:16:53 -0700

    Updates

---
+++ precache-manifest.1a94fc35d4c83d7b8c9ec0938bd52600.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b24cce545ccdd1d859b0ae4fe2fc21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ede499cba59cc1eb9825",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "75149f96cd0023aa3025",
+    "url": "/static/css/main.d92f661a.chunk.css"
+  },
+  {
+    "revision": "ede499cba59cc1eb9825",
+    "url": "/static/js/2.d72fb1d6.chunk.js"
+  },
+  {
+    "revision": "451ffe720aa8add9f554b8ed54913322",
+    "url": "/static/js/2.d72fb1d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75149f96cd0023aa3025",
+    "url": "/static/js/main.4e5d4594.chunk.js"
+  },
+  {
+    "revision": "347ac703bce0c3f2133b",
+    "url": "/static/js/runtime-main.691efcb0.js"
+  },
+  {
+    "revision": "fc9dc010851399f152353e7d9d50edfe",
+    "url": "/static/media/Resume (2).fc9dc010.pdf"
+  },
+  {
+    "revision": "31d89501b216b06428bd544937b57f26",
+    "url": "/static/media/Screen Shot 2020-10-19 at 5.18.23 PM.31d89501.png"
+  },
+  {
+    "revision": "7c9932f9226f61d78b56a1219804a680",
+    "url": "/static/media/Screen Shot 2020-10-19 at 7.26.10 PM.7c9932f9.png"
+  },
+  {
+    "revision": "350ab53795a00ea2d099a5545c0fb43a",
+    "url": "/static/media/purty_wood.350ab537.png"
+  },
+  {
+    "revision": "d7303f352bac9ff5271f0de2b37509af",
+    "url": "/static/media/sjsu_logo.d7303f35.png"
+  },
+  {
+    "revision": "75edce1c545f3329b3c3b71af166ecce",
+    "url": "/static/media/ucsc_logo.75edce1c.png"
+  }
+]);
