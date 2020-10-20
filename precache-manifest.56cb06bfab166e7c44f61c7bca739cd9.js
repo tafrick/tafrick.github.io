cf641813b38394eb01b2d26d9bebda975e473cdd
Tyler Frick <tyfrick25@gmail.com>
2020-10-19 23:41:39 -0700

    Updates

---
+++ precache-manifest.56cb06bfab166e7c44f61c7bca739cd9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b24a1ed130b78a333621f3b8edf4bf",
+    "url": "/tafrick.github.io/index.html"
+  },
+  {
+    "revision": "ede499cba59cc1eb9825",
+    "url": "/tafrick.github.io/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "a3e826c4d30aeb74f3ba",
+    "url": "/tafrick.github.io/static/css/main.047f7272.chunk.css"
+  },
+  {
+    "revision": "ede499cba59cc1eb9825",
+    "url": "/tafrick.github.io/static/js/2.d72fb1d6.chunk.js"
+  },
+  {
+    "revision": "451ffe720aa8add9f554b8ed54913322",
+    "url": "/tafrick.github.io/static/js/2.d72fb1d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3e826c4d30aeb74f3ba",
+    "url": "/tafrick.github.io/static/js/main.d7196780.chunk.js"
+  },
+  {
+    "revision": "6fe88a55d95e367e61b6",
+    "url": "/tafrick.github.io/static/js/runtime-main.ce2a7c7f.js"
+  },
+  {
+    "revision": "fc9dc010851399f152353e7d9d50edfe",
+    "url": "/tafrick.github.io/static/media/Resume (2).fc9dc010.pdf"
+  },
+  {
+    "revision": "31d89501b216b06428bd544937b57f26",
+    "url": "/tafrick.github.io/static/media/Screen Shot 2020-10-19 at 5.18.23 PM.31d89501.png"
+  },
+  {
+    "revision": "7c9932f9226f61d78b56a1219804a680",
+    "url": "/tafrick.github.io/static/media/Screen Shot 2020-10-19 at 7.26.10 PM.7c9932f9.png"
+  },
+  {
+    "revision": "350ab53795a00ea2d099a5545c0fb43a",
+    "url": "/tafrick.github.io/static/media/purty_wood.350ab537.png"
+  },
+  {
+    "revision": "d7303f352bac9ff5271f0de2b37509af",
+    "url": "/tafrick.github.io/static/media/sjsu_logo.d7303f35.png"
+  },
+  {
+    "revision": "75edce1c545f3329b3c3b71af166ecce",
+    "url": "/tafrick.github.io/static/media/ucsc_logo.75edce1c.png"
+  }
+]);
