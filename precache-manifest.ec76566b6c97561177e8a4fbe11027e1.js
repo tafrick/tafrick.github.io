9c9091dc84c465b6cd8c12904fd28236c475faeb
Tyler Frick <tyfrick25@gmail.com>
2020-10-20 10:57:17 -0700

    Updates

---
+++ precache-manifest.ec76566b6c97561177e8a4fbe11027e1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adefb86cf50a12d824590480659a2ed4",
+    "url": "/tafrick.gihub.io/index.html"
+  },
+  {
+    "revision": "836ab19d4afe5a6946f4",
+    "url": "/tafrick.gihub.io/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "337b81c9ac54d75a89ea",
+    "url": "/tafrick.gihub.io/static/css/main.7494ff26.chunk.css"
+  },
+  {
+    "revision": "836ab19d4afe5a6946f4",
+    "url": "/tafrick.gihub.io/static/js/2.0a77d7c4.chunk.js"
+  },
+  {
+    "revision": "7fa8ec16d0a9f9102dca5c04818f2334",
+    "url": "/tafrick.gihub.io/static/js/2.0a77d7c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "337b81c9ac54d75a89ea",
+    "url": "/tafrick.gihub.io/static/js/main.24adcb3e.chunk.js"
+  },
+  {
+    "revision": "26806dd3ee426cb40b67",
+    "url": "/tafrick.gihub.io/static/js/runtime-main.464e19ba.js"
+  },
+  {
+    "revision": "fc9dc010851399f152353e7d9d50edfe",
+    "url": "/tafrick.gihub.io/static/media/Resume (2).fc9dc010.pdf"
+  },
+  {
+    "revision": "31d89501b216b06428bd544937b57f26",
+    "url": "/tafrick.gihub.io/static/media/Screen Shot 2020-10-19 at 5.18.23 PM.31d89501.png"
+  },
+  {
+    "revision": "7c9932f9226f61d78b56a1219804a680",
+    "url": "/tafrick.gihub.io/static/media/Screen Shot 2020-10-19 at 7.26.10 PM.7c9932f9.png"
+  },
+  {
+    "revision": "350ab53795a00ea2d099a5545c0fb43a",
+    "url": "/tafrick.gihub.io/static/media/purty_wood.350ab537.png"
+  },
+  {
+    "revision": "8ed5781b65c580d35cfda9741be8c13e",
+    "url": "/tafrick.gihub.io/static/media/purty_wood_@2X.8ed5781b.png"
+  },
+  {
+    "revision": "d7303f352bac9ff5271f0de2b37509af",
+    "url": "/tafrick.gihub.io/static/media/sjsu_logo.d7303f35.png"
+  },
+  {
+    "revision": "75edce1c545f3329b3c3b71af166ecce",
+    "url": "/tafrick.gihub.io/static/media/ucsc_logo.75edce1c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1947a45b8dade16f56b78da3fa81081e.js"
+  "/tafrick.gihub.io/precache-manifest.ec76566b6c97561177e8a4fbe11027e1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tafrick.gihub.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
