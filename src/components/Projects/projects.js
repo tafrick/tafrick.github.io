93a7dc71a4df56d68fe2e50edd40d32bf42ff8ab
Tyler Frick <tyfrick25@gmail.com>
2020-10-20 16:06:05 -0700

    major updates with about me

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -13,30 +13,30 @@ import { Carousel } from 'react-responsive-carousel';
 const projects = (skills) => {
     return (
         <div className={classes.ProjectsWrapper} id="Content">
-            <h1>My Projects:</h1>
+            {/* <h1>My Projects:</h1>
             <div className={classes.Project}>
-                <h2>Web Dev</h2>
-                <Carousel>
-                    {/* <div>
+                <h2>Web Dev</h2> */}
+            <Carousel>
+                {/* <div>
                         <img src={DLA} alt="DLA" />
                         <p>DuelLinksAcademy.xyz"</p>
                     </div>
                     <img src={BB} alt="burger-builder" />
                     <p>BurgerBuilder</p> */}
-                    <Project
-                        title="DuelLinksAcademy.xyz"
-                        source={DLA} alt="DLA"
-                        clicked={() => window.open("https://duellinksacademy.xyz")}
-                        clicked2={() => window.open("https://github.com/tafrick/DuelLinks-Site")}
-                        description="A card searching website for mobile game, with deck building functionality and community elements" />
-                    <Project
-                        title="BurgerBuilder"
-                        source={BB} alt="BurgerBuilder"
-                        clicked={() => window.open("https://react-my-burger-58f16.web.app/")}
-                        clicked2={() => window.open("https://github.com/tafrick/Burger-Builder-Udemy-")}
-                        description="Udemy course project used to learn React" />
-                </Carousel>
-            </div>
+                <Project
+                    title="DuelLinksAcademy.xyz"
+                    source={DLA} alt="DLA"
+                    clicked={() => window.open("https://duellinksacademy.xyz")}
+                    clicked2={() => window.open("https://github.com/tafrick/DuelLinks-Site")}
+                    description="A card searching website for mobile game, with deck building functionality and community elements" />
+                <Project
+                    title="BurgerBuilder"
+                    source={BB} alt="BurgerBuilder"
+                    clicked={() => window.open("https://react-my-burger-58f16.web.app/")}
+                    clicked2={() => window.open("https://github.com/tafrick/Burger-Builder-Udemy-")}
+                    description="Udemy course project used to learn React" />
+            </Carousel>
+            {/* </div> */}
             <Arrow Linked="section4" />
             {/* <div className={classes.Project}>
                 <h2>Cyber Security</h2>
